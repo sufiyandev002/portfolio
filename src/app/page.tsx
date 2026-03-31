cf5982d8873898f29e7f1d500f531bb81e56ce28
sufiyan <sufiyan.dev002@gmail.com>
2026-03-31 14:36:54 +0500

    commented out a section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <Skills />
       <Services />
       <Portfolio />
-      <BestThing />
+      {/* <BestThing /> */}
       <HowIWork />
       <Cta />
     </main>
